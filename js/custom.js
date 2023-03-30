db4f879c01017cfe26ff58e39a8253a7f5274df4
Asemota Ayevbosa <asemotamaki1@gmail.com>
2023-03-30 13:14:09 +0000

    preloader

--- js/custom.js
+++ js/custom.js
@@ -4,14 +4,9 @@ $(window).on('load', function() {
     /*=========================================================================
         Preloader
     =========================================================================*/
-    $("#preloader").delay(350).fadeOut('slow');
-    // Because only Chrome supports offset-path, feGaussianBlur for now
-    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-
-    if(!isChrome) {
-        document.getElementsByClassName('infinityChrome')[0].style.display = "none";
-        document.getElementsByClassName('infinity')[0].style.display = "block";
-    }
+    $(window).load(function() {
+      $(".loader-overlay").fadeOut("slow");
+    })
 
     /*=========================================================================
      Wow Initialize
